877e0c5e9a6dad3833ba016f4eabb6c41e02c27f
k.sakai <k.sakai@eneres.co.jp>
2015-12-01 17:22:08 +0900

    wip show error message

---
+++ scripts/RtValidateModel.js
@@ -0,0 +1,92 @@
+/**
+ * バリデートmodel
+ */
+function RtValidateModel(attrs) {
+    this.val = '';
+
+    this.listeners = {
+        valid: [],
+        invalid: []
+    };
+
+    this.attrs = {};
+    this.terms = {
+        required: '',
+        maxlength: 0,
+        minlength: 0,
+        //number: /^[-]?[0-9]+(?.[0-9]+)?$/,
+        number: /^([1-9]\d*|0)(\.\d+)?$/,
+        url: /^(https?|ftp)(:\/\/[-_.!~*?'()a-zA-Z0-9;?/?:?@&=+?$,%#]+)$/,
+        mail: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    };
+
+    if (attrs.required !== undefined) this.attrs['required'] = attrs.required;
+    if (attrs.minlength !== undefined) this.attrs['minlength'] = attrs.minlength;
+    if (attrs.maxlength !== undefined) this.attrs['maxlength'] = attrs.maxlength;
+    if (attrs.number !== undefined) this.attrs['number'] = this.terms.number;
+    if (attrs.url !== undefined) this.attrs['url'] = this.terms.url;
+    if (attrs.mail !== undefined) this.attrs['mail'] = this.terms.mail;
+}
+
+RtValidateModel.prototype.set = function(val) {
+    this.val = val;
+    this.validate();
+};
+
+RtValidateModel.prototype.validate = function() {
+    var val;
+    // バリデーションでエラーが出たものを保存しておく配列を用意
+    this.errors = [];
+
+    for (var key in this.attrs) {
+        val = this.attrs[key];
+        if (!this[key](val)) {this.errors.push(key); break;}
+    }
+
+    this.trigger(!this.errors.length ? 'valid' : 'invalid');
+};
+
+RtValidateModel.prototype.required = function() {
+    return this.val === '';
+};
+
+RtValidateModel.prototype.minlength = function(rule) {
+    if (this.val === '') return true;
+    return rule <= this.val.length;
+};
+
+RtValidateModel.prototype.maxlength = function(rule) {
+    if (this.val === '') return true;
+    return rule >= this.val.length;
+};
+
+RtValidateModel.prototype.number = function(rule) {
+    if (this.val === '') return true;
+    return this.val.match(rule);
+};
+
+RtValidateModel.prototype.url = function(rule) {
+    if (this.val === '') return true;
+    return this.val.match(rule);
+};
+
+RtValidateModel.prototype.mail = function(rule) {
+    if (this.val === '') return true;
+    return this.val.match(rule);
+};
+
+/**
+ * オブザーバーの振る舞いを設定
+ */
+RtValidateModel.prototype.on = function(event, func) {
+    this.listeners[event].push(func);
+};
+
+/**
+ * オブザーバーのイベントを発火
+ */
+RtValidateModel.prototype.trigger = function(event) {
+    $.each(this.listeners[event], function() {
+        this();
+    });
+};
---
+++ scripts/RtValidateView.js
@@ -0,0 +1,115 @@
+/**
+ * バリデートview
+ * @param {el} 対象要素
+ * @param {prefix} 使用プレフィックス
+ */
+function RtValidateView (el, prefix) {
+    // prefix
+    this.prefix = 'rv';
+    if (prefix !== undefined) this.prefix = prefix;
+    // 初期化
+    this.initialize(el);
+    // イベント
+    this.handleEvents();
+}
+
+RtValidateView.prototype.initialize = function(el) {
+    this.$el = $(el);
+    this.$form = this.$el.parents('form');
+    this.formKey = this.$form.data(this.prefix + '-form');
+    this.validationKey = this.$el.data(this.prefix + '-validate');
+    this.$submit = $('.js-submit');
+    this.attrs = {};
+    var self = this;
+
+    // エラーメッセージ非表示
+    $('[data-' + self.prefix
+               + '-error^="'
+               + self.formKey + '.'
+               + self.validationKey + '."]'
+    ).hide();
+
+    // HTML5のrequiredがあれば必須項目を設定する
+    if(this.$el.prop('required')) {
+        self.attrs["required"] = "";
+    }
+
+    // prefixから対象の属性のみ取得し、prefixを削除する
+    $.each(this.$el.data(), function(index, val) {
+        // prefixが付いている属性を対象とする
+        var num = index.indexOf(self.prefix, 0);
+        if (num === 0) {
+            index = index.toLowerCase().substring(
+                                            num + self.prefix.length,
+                                            index.length
+                                        );
+            self.attrs[index] = val;
+        }
+    });
+
+    this.model = new RtValidateModel(self.attrs);
+};
+
+RtValidateView.prototype.handleEvents = function()  {
+    var self = this;
+    this.$el.on('keyup', function(e) {
+        self.onKeyup(e);
+    });
+
+    this.$submit.on('click', function() {
+        self.onClick();
+    });
+
+    this.model.on('valid', function() {
+        self.onValid();
+    });
+
+    this.model.on('invalid', function() {
+        self.onInvalid();
+    });
+};
+
+RtValidateView.prototype.onKeyup = function(e) {
+    var $target = $(e.currentTarget);
+    this.model.set($target.val());
+};
+
+RtValidateView.prototype.onClick = function() {
+    var $target = this.$el;
+
+    if($target.hasClass('js-validate-required')){
+        this.model.set($target.val());
+    }
+
+};
+
+RtValidateView.prototype.onValid = function() {
+    var $targetForm = this.$form;
+
+    // エラーメッセージを非表示にする
+    $('[data-' + this.prefix
+               + '-error^="'
+               + this.formKey + '.'
+               + this.validationKey + '."]'
+    ).hide();
+
+    this.$submit.click(function(){
+        $targetForm.submit();
+    });
+};
+
+RtValidateView.prototype.onInvalid = function() {
+    // show error message
+    $('[data-' + this.prefix
+               + '-error="'
+               + this.formKey + '.'
+               + this.validationKey + '.'
+               + this.model.errors[0] + '"]'
+    ).show();
+
+    this.setInvalid();
+};
+
+RtValidateView.prototype.setInvalid = function() {
+    //this.$el.addClass('error');
+};
